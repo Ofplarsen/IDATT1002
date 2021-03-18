8f2f1cf4b933a65fc8f93f8e54eb1f8e4296f889
Olav Finne Præsteng Larsen <oflarsen@stud.ntnu.no>
2021-03-18 15:30:41 +0100

    Made changes to pom file and added test to SubprojectTest

--- src/test/java/edu/ntnu/idatt1002/k2_2/mitodo/data/SubprojectTest.java
+++ src/test/java/edu/ntnu/idatt1002/k2_2/mitodo/data/SubprojectTest.java
@@ -1,29 +1,72 @@
 package edu.ntnu.idatt1002.k2_2.mitodo.data;
 
+import edu.ntnu.idatt1002.k2_2.mitodo.controller.DataManager;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubprojectTest {
 
-    @Test
-    void setTitle() {
+    Subproject ntnuMatte = new Subproject("NTNU Matte", Color.LIGHTBLUE);
+
+    @Nested
+    @DisplayName("Tests to test setTitle method")
+    class setTitle{
+
+        @Test
+        void setTitleTrue() {
+            assertTrue(ntnuMatte.getTitle().equals("NTNU Matte"));
+            ntnuMatte.setTitle("NTNU Prog");
+            assertEquals(ntnuMatte.getTitle(), "NTNU Prog");
+        }
+
+        @Test
+        void setTitleFalse() {
+            assertTrue(ntnuMatte.getTitle().equals("NTNU Matte"));
+            ntnuMatte.setTitle("NTNU Prog");
+            assertNotEquals(ntnuMatte.getTitle(), "NTNU Pro");
+        }
+    }
+    @Nested
+    @DisplayName("Tests for getTitle method")
+    class getTitle{
+
+        @Test
+        void getTitleEquals() {
+            assertEquals(ntnuMatte.getTitle(), "NTNU Matte");
+        }
+
+        @Test
+        void getTitleDoesNotEquals(){
+            assertNotEquals(ntnuMatte.getTitle(), "NTNU Prog");
+        }
+    }
+    @Nested
+    @DisplayName("Tests for setColor and getColor methods")
+    class setColorAndGetColor{
+        @Test
+        void setColorAndGetColorTrue() {
+            assertEquals(ntnuMatte.getColor(), Color.LIGHTBLUE);
+            ntnuMatte.setColor(Color.LIGHTPINK);
+            assertEquals(ntnuMatte.getColor(), Color.LIGHTPINK);
+        }
+
+        @Test
+        void setColorAndGetColorFalse(){
+            assertNotEquals(ntnuMatte.getColor(), Color.LIGHTPINK);
+            ntnuMatte.setColor(Color.LIGHTPINK);
+            assertNotEquals(ntnuMatte.getColor(), Color.LIGHTBLUE);
+        }
     }
 
-    @Test
-    void getTitle() {
-    }
-
-    @Test
-    void setColor() {
-    }
-
-    @Test
-    void getColor() {
-    }
 
     @Test
     void addTask() {
+
     }
 
     @Test
