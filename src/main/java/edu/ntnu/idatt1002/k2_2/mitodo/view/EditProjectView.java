b6b64c8a602f61dcfa1578f37051dd3ed9a98462
Erlend <erlendmatre10@gmail.com>
2021-04-12 23:50:06 +0200

    Fixed exit button on EditProjectView.java

--- src/main/java/edu/ntnu/idatt1002/k2_2/mitodo/view/EditProjectView.java
+++ src/main/java/edu/ntnu/idatt1002/k2_2/mitodo/view/EditProjectView.java
@@ -32,13 +32,16 @@ public class EditProjectView extends View{
         exit();
     }
 
-    public void exit(){
-        Client.setView("ProjectView");
+    public void exit(){ //Returns to the same project
+        ProjectView projectView = (ProjectView) Client.setView("ProjectView");
+        projectView.setProject(project);
     }
 
-    public void delete(){
+    public void delete(){ //denna fjerne ikkje prosjektet forresten, må sikkert gå via rootProject for å fjerna
         project.removeProject(project.getID());
-        exit();
+        //exit();
+        ProjectView projectView = (ProjectView) Client.setView("ProjectView"); //returns to quick tasks because it can not return to deleted project
+        projectView.setProject(Client.getQuickTasks());
     }
 
     public Node getParent()
