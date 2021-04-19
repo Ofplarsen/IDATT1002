2ebbe48649297db6378ce45b45084092813244a4
William G. Tresselt <williagt@stud.ntnu.no>
2021-04-19 15:12:29 +0200

    Fixed bug related to sorting.

--- src/main/java/edu/ntnu/idatt1002/k2_2/mitodo/data/TaskListSorter.java
+++ src/main/java/edu/ntnu/idatt1002/k2_2/mitodo/data/TaskListSorter.java
@@ -2,6 +2,7 @@ package edu.ntnu.idatt1002.k2_2.mitodo.data;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 
@@ -22,13 +23,9 @@ public class TaskListSorter {
     }
 
     public static void sortByDueDate(ArrayList<Task> tasks, boolean increasing) {
-        ArrayList<Task> noDueDate = new ArrayList<>();
+        ArrayList<Task> noDueDate = (ArrayList<Task>) tasks.stream().filter(t -> t.getDueDate() == null).collect(Collectors.toList());
 
-        for (int i = tasks.size() - 1; i >= 0; i -= 1) {
-            if (tasks.get(i).getDueDate() == null) {
-                noDueDate.add(tasks.remove(i));
-            }
-        }
+        tasks.removeAll(noDueDate);
 
         Comparator<Task> dueDateComparator;
         if (increasing) {
@@ -42,13 +39,9 @@ public class TaskListSorter {
     }
 
     public static void sortByStartDate(ArrayList<Task> tasks, boolean increasing) {
-        ArrayList<Task> noStartDate = new ArrayList<>();
+        ArrayList<Task> noStartDate = (ArrayList<Task>) tasks.stream().filter(t -> t.getStartDate() == null).collect(Collectors.toList());
 
-        for (int i = tasks.size() - 1; i >= 0; i -= 1) {
-            if (tasks.get(i).getStartDate() == null) {
-                noStartDate.add(tasks.remove(i));
-            }
-        }
+        tasks.removeAll(noStartDate);
 
         Comparator<Task> startDateComparator;
         if (increasing) {
